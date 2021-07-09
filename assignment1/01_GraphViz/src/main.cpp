c74d4840d297f168706bbe76fb9a15581c9390f4
khy <khydom@qq.com>
2021-07-09 14:58:36 +0800

    assignment1 welcome

--- assignment1/01_GraphViz/src/main.cpp
+++ assignment1/01_GraphViz/src/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "SimpleGraph.h"
+#include <string>
 
 using std::cout;	using std::endl;
 
 void Welcome();
-
+template <typename T>
+T PromoteInfo();
+SimpleGraph ReadFile(std::string &name);
 // Main method
 int main() {
     Welcome();
+    cout<<"input the file name(one in 'res' folder): \n";
+    std::string filename=PromoteInfo();
+    cout<<"input the number of seconds to run the algorithm(a positive number): \n";
+    double run_time=PromoteInfo();
+    SimpleGraph graph=ReadFile(filename);
     return 0;
 }
 
@@ -18,4 +26,15 @@ void Welcome() {
     cout << "This program uses a force-directed graph layout algorithm" << endl;
     cout << "to render sleek, snazzy pictures of various graphs." << endl;
     cout << endl;
+}
+template <typename T>
+T PromoteInfo()
+{
+    T res;
+    std::cin>>res;
+    return res;
+}
+SimpleGraph ReadFile(std::string &name)
+{
+    
 }
